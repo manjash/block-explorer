da8bda849f99c4a4cb6904b1bc15b9bee68ad077
Johann Kerbrat <johann@kerbrat.eu>
2020-11-11 16:16:48 -0800

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+  },
+  plugins: ["prettier", "react", "@typescript-eslint"],
+  rules: {},
+};
