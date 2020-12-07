2cb4710aeb80a42fb6ec43991b8c3540980299de
Johann Kerbrat <johann@kerbrat.eu>
2020-12-07 14:24:58 -0800

    Scroll to top on mobile

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import ScrollToTop from 'react-router-scroll-top'
 
 import Layout from '../Layout/Layout'
 import routes from '../../routes/routeSwitch'
@@ -8,16 +9,20 @@ import routes from '../../routes/routeSwitch'
 const App = () => (
   <HelmetProvider>
     <Router>
-      <Layout routes={routes}>
-        <Switch>
-          {routes
-            .filter((route) => route.path !== undefined)
-            .map((route) => {
-              return <Route path={route.path} component={route.component} key={route.nameKey} />
-            })}
-          <Redirect from='/' to='dashboard' />
-        </Switch>
-      </Layout>
+      <ScrollToTop>
+        <Layout routes={routes}>
+          <Switch>
+            {routes
+              .filter((route) => route.path !== undefined)
+              .map((route) => {
+                return (
+                  <Route path={route.path} component={route.component} key={route.nameKey} />
+                )
+              })}
+            <Redirect from='/' to='dashboard' />
+          </Switch>
+        </Layout>
+      </ScrollToTop>
     </Router>
   </HelmetProvider>
 )
