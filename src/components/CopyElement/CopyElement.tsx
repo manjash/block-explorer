d7cfcf68738982740e7b98033410abb6fe4d59e2
Johann Kerbrat <johann@kerbrat.eu>
2021-02-09 16:08:51 -0800

    responsive fix

--- src/components/CopyElement/CopyElement.tsx
+++ src/components/CopyElement/CopyElement.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { makeStyles } from '@material-ui/core/styles'
+import { useTheme, makeStyles } from '@material-ui/core/styles'
 import Alert from '@material-ui/lab/Alert'
 import Snackbar from '@material-ui/core/Snackbar'
 import Tooltip from '@material-ui/core/Tooltip'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 
 import copyElementStyle from '../../assets/jss/components/CopyElement/copyElementStyle'
 import copyIcon from '../../assets/images/copy.svg'
@@ -18,8 +19,10 @@ const useStyles = makeStyles(copyElementStyle)
 
 const CopyElement = ({ copyString, children }: Prop) => {
   const { t } = useTranslation()
+  const theme = useTheme()
   const classes = useStyles()
   const [display, setDisplay] = useState(false)
+  const isSmallBreakpoint = useMediaQuery(theme.breakpoints.down('sm'))
 
   const handleClose = (event: any, reason: any) => {
     if (reason === 'clickaway') {
@@ -36,7 +39,11 @@ const CopyElement = ({ copyString, children }: Prop) => {
     <>
       {display && (
         <Snackbar open={display} onClose={handleClose} autoHideDuration={2000}>
-          <Alert severity='info'>{t('app.navigation.copySuccess', { copyString })}</Alert>
+          <Alert severity='info'>
+            {t('app.navigation.copySuccess', {
+              copyString: isSmallBreakpoint === true ? null : copyString,
+            })}
+          </Alert>
         </Snackbar>
       )}
       <div onClick={copyClick} className={classes.root}>
