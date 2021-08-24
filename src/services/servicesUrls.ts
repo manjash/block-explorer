2efd9f7f5117e81d0c3d6165900ec608f10db4af
adcrn <9167686+adcrn@users.noreply.github.com>
2021-08-24 15:08:22 -0700

    IRO-1009: Adjust block detail and block search pages

--- src/services/servicesUrls.ts
+++ src/services/servicesUrls.ts
@@ -2,9 +2,9 @@ import { API_HOST } from '../config'
 
 // TODO improve URLs the config
 export enum ApiUrls {
-  BLOCK_DETAIL_PAGE = '/block',
+  BLOCK_DETAIL_PAGE = '/blocks/find?',
   BLOCK_TRANSACTION_PAGE = '/block/transaction',
-  SEARCH_BLOCKS = '/search/blocks',
+  SEARCH_BLOCKS = '/blocks?',
   SEARCH_TRANSACTIONS = '/search/transactions',
 }
 
