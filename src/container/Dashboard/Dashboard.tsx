1f2db6dc6171978266fff34755dcd4b2be6eb8af
Alexander Decurnou <alex@deekerno.com>
2021-09-07 14:41:34 -0400

    IRO-1043: Adjust dashboard to use new block type

--- src/container/Dashboard/Dashboard.tsx
+++ src/container/Dashboard/Dashboard.tsx
@@ -42,12 +42,12 @@ const Dashboard = () => {
   }
 
   if (blockData && blockData.length > 0) {
-    metrics.transactionsCount = blockData[0].metadata.transactionsCount || null
+    metrics.transactionsCount = blockData[0].transactionsCount || null
     metrics.blockTime =
       (blockData[0].timestamp.getTime() - blockData[1].timestamp.getTime()) / 1000
-    metrics.latestHash = blockData[0].block_identifier.hash || null
-    metrics.height = blockData[0].block_identifier.index || null
-    metrics.difficulty = blockData[0].metadata.difficulty || null
+    metrics.latestHash = blockData[0].hash || null
+    metrics.height = blockData[0].sequence || null
+    metrics.difficulty = blockData[0].difficulty || null
   }
 
   return (
