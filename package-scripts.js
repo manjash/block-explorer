5f4e5337a144653a1d97ee9ceb681f48471604e1
brekk <brekk@brekkbockrath.com>
2021-12-14 15:18:34 -0800

    add nps

---
+++ package-scripts.js
@@ -0,0 +1,12 @@
+module.exports = {
+  scripts: {
+    start: 'react-scripts start',
+    dev: 'REACT_APP_API_HOST=http://localhost:8003 react-scripts start',
+    build: 'react-scripts build',
+    test: 'react-scripts test',
+    eject: 'react-scripts eject',
+    type: `tsc -b`,
+    lint: 'eslint --ext .ts,.tsx,.jsx,.jsx src --fix',
+    care: `nps build type lint test`,
+  },
+}
