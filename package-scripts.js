17a06a5cdd81acabd8ab67e0dcbd60ecc1ef7e22
brekk <brekk@brekkbockrath.com>
2021-12-15 11:43:45 -0800

    add hooks

--- package-scripts.js
+++ package-scripts.js
@@ -3,10 +3,12 @@ module.exports = {
     start: 'react-scripts start',
     dev: 'REACT_APP_API_HOST=http://localhost:8003 react-scripts start',
     build: 'react-scripts build',
-    test: 'react-scripts test',
+    test: {
+      watch: 'react-scripts test',
+    },
     eject: 'react-scripts eject',
     type: `tsc -b`,
     lint: 'eslint --ext .ts,.tsx,.jsx,.jsx src --fix',
-    care: `nps build type lint test`,
+    care: `nps build type lint`,
   },
 }
