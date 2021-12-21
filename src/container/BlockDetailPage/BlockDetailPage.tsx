manjash/block-explorer

commit d16f39fe96f830e1bf5600bb28f5de52c724db34

IRO-1043: Adjust fields references

src/container/BlockDetailPage/BlockDetailPage.tsx
@@ -46,30 +46,30 @@ const BlockDetailPage = () => {
   )
 
   const blockData = service.status === ServiceState.LOADED && service.payload.result
+  if (!blockData) return null
+  const { sequence, hash, transactions, size, difficulty, timestamp } = blockData
   const metaVariables = {
-    id: blockData ? `${blockData.sequence}` : '',
-    hash: blockData ? blockData.hash : '',
+    id: sequence,
+    hash: hash,
   }
 
   return (
     <Container>
-      {blockData && <Meta path={RoutePath.BlockDetailPage} variables={metaVariables} />}
+      <Meta path={RoutePath.BlockDetailPage} variables={metaVariables} />
 
-      {blockData && (
-        <Breadcrumb
-          paths={[
-            {
-              title: t('app.components.breadcrumb.explorer'),
-              to: RoutePath.Explorer,
-              logo: blocks,
-            },
-            {
-              title: getDisplayShortHash(blockData.hash || ''),
-              logo: blocksGray,
-            },
-          ]}
-        />
-      )}
+      <Breadcrumb
+        paths={[
+          {
+            title: t('app.components.breadcrumb.explorer'),
+            to: RoutePath.Explorer,
+            logo: blocks,
+          },
+          {
+            title: getDisplayShortHash(blockData.hash || ''),
+            logo: blocksGray,
+          },
+        ]}
+      />
 
       <BoxWrapper
         isLoading={service.status === ServiceState.LOADING}
@@ -82,21 +82,21 @@ const BlockDetailPage = () => {
           />
         )}
         <div>
-          {blockData && blockData.transactions && (
+          {transactions && (
             <InformationPanel
-              height={blockData.sequence}
-              blockHash={blockData.hash}
-              size={getDisplaySizeInBytes(blockData.size)}
-              transactions={blockData.transactions.length}
-              difficulty={blockData.difficulty}
-              timestamp={blockData.timestamp}
+              height={sequence}
+              blockHash={hash}
+              size={getDisplaySizeInBytes(size)}
+              transactions={transactions.length}
+              difficulty={difficulty}
+              timestamp={timestamp}
             />
           )}
         </div>
       </BoxWrapper>
 
-      {blockData && blockData.transactions && blockData.transactions.length > 0 && (
-        <TransactionsList transactions={blockData.transactions} blockHash={blockData.hash} />
+      {transactions && transactions.length > 0 && (
+        <TransactionsList transactions={transactions} blockHash={hash} />
       )}
     </Container>
   )

commit e161c3c3915e5e242438c89b5a05e29b7077806a

Fix the block explorer 404 error (#64)
The page was broken after commit
https://github.com/iron-fish/block-explorer/commit/d16f39fe96f830e1bf5600bb28f5de52c724db34
but this fixes it by reverting many of the changes in that PR. It was
made without looking at the rest of the page to see that it relied
on loading even if the data was not loaded.

src/container/BlockDetailPage/BlockDetailPage.tsx
@@ -46,38 +46,46 @@ const BlockDetailPage = () => {
   )
 
   const blockData = service.status === ServiceState.LOADED && service.payload.result
-  if (!blockData) return null
-  const { sequence, hash, transactions, graffiti, difficulty, timestamp, main } = blockData
-  const metaVariables = {
-    id: sequence,
-    hash: hash,
-  }
 
   return (
     <Container>
-      <Meta path={RoutePath.BlockDetailPage} variables={metaVariables} />
-      <Breadcrumb
-        paths={[
-          {
-            title: t('app.components.breadcrumb.explorer'),
-            to: RoutePath.Explorer,
-            logo: blocks,
-          },
-          {
-            title: getDisplayShortHash(blockData.hash || ''),
-            logo: blocksGray,
-          },
-        ]}
-      />
+      {blockData && (
+        <Meta
+          path={RoutePath.BlockDetailPage}
+          variables={{
+            id: blockData.sequence,
+            hash: blockData.hash,
+          }}
+        />
+      )}
+
+      {blockData && (
+        <Breadcrumb
+          paths={[
+            {
+              title: t('app.components.breadcrumb.explorer'),
+              to: RoutePath.Explorer,
+              logo: blocks,
+            },
+            {
+              title: getDisplayShortHash(blockData.hash || ''),
+              logo: blocksGray,
+            },
+          ]}
+        />
+      )}
+
       <BoxWrapper
         isLoading={service.status === ServiceState.LOADING}
         header={
-          <>
-            {t('app.blockDetailPage.information.title')}{' '}
-            {!main && (
-              <SmallChip text={t('app.blockDetailPage.information.forked')}></SmallChip>
-            )}
-          </>
+          blockData && (
+            <>
+              {t('app.blockDetailPage.information.title')}{' '}
+              {!blockData.main && (
+                <SmallChip text={t('app.blockDetailPage.information.forked')}></SmallChip>
+              )}
+            </>
+          )
         }
       >
         {service.status === ServiceState.ERROR && (
@@ -86,21 +94,24 @@ const BlockDetailPage = () => {
             description={t('app.blockDetailPage.information.error.description')}
           />
         )}
-        <div>
-          {transactions && (
-            <InformationPanel
-              height={sequence}
-              blockHash={hash}
-              graffiti={truncateGraffitiToLimit(graffiti)}
-              transactions={transactions.length}
-              difficulty={difficulty}
-              timestamp={timestamp}
-            />
-          )}
-        </div>
+        {blockData && (
+          <div>
+            {blockData.transactions && (
+              <InformationPanel
+                height={blockData.sequence}
+                blockHash={blockData.hash}
+                graffiti={truncateGraffitiToLimit(blockData.graffiti)}
+                transactions={blockData.transactions.length}
+                difficulty={blockData.difficulty}
+                timestamp={blockData.timestamp}
+              />
+            )}
+          </div>
+        )}
       </BoxWrapper>
-      {transactions && transactions.length > 0 && (
-        <TransactionsList transactions={transactions} blockHash={hash} />
+
+      {blockData && blockData.transactions && blockData.transactions.length > 0 && (
+        <TransactionsList transactions={blockData.transactions} blockHash={blockData.hash} />
       )}
     </Container>
   )